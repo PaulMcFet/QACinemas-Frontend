b71bb9ed75ef50c292fe5651948f2538419dd1c4
Lee <leenock2012@yahoo.co.uk>
2022-05-17 13:22:15 +0100

    Added  a Navigation bar that will persist throughout pages

--- qa-cinemas/src/App.jsx
+++ qa-cinemas/src/App.jsx
@@ -1,22 +1,11 @@
 import "./App.css";
-import Logo from "./Assets/qa-logo.jpg";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { Navbar } from "react-bootstrap";
+import Navbar from "./components/Nav/Navbar";
 
 function App() {
   return (
     <div className="App">
-      <Navbar bg="dark" variant="dark" sticky="true">
-        <Navbar.Brand>
-          <img
-            src={Logo}
-            width="90"
-            height="90"
-            className="d-inline-block align-top"
-            alt="QA Cinemas Logo"
-          />
-        </Navbar.Brand>
-      </Navbar>
+      <Navbar />
     </div>
   );
 }
