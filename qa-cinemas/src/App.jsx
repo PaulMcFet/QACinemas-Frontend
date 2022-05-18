be635dd9b3c27a9adcc26f39117639e9ff8b0211
Lee <leenock2012@yahoo.co.uk>
2022-05-18 11:19:32 +0100

    Added changes for home page

--- qa-cinemas/src/App.jsx
+++ qa-cinemas/src/App.jsx
@@ -3,13 +3,14 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import Navbar from "./components/Nav/Navbar";
 import React from 'react';
 import Footer from './components/Footer/Footer';
+import Home from './components/Home/Home';
 
 function App() {
 
   return (
     <div className="App">
       <Navbar />
-
+      <Home />
       <Footer />
     </div>
   );
