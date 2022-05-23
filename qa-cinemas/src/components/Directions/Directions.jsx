38baee573ec2cb571aef8fa0a0e784b15eed6626
Lee <leenock2012@yahoo.co.uk>
2022-05-23 15:07:57 +0100

    Added Directions
    Made a start on Directions page - issue with Google Maps API key not being picked up from the code

--- qa-cinemas/src/components/Directions/Directions.jsx
+++ qa-cinemas/src/components/Directions/Directions.jsx
@@ -1,9 +1,44 @@
-import React from 'react'
+import React from "react";
+import { useMemo } from "react";
+import { GoogleMap, useLoadScript, Marker } from "@react-google-maps/api";
+import { Wrapper } from "@googlemaps/react-wrapper";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import './directions.css'
 
 const Directions = () => {
-  return (
-    <div>Directions</div>
-  )
-}
+  const { isLoaded } = useLoadScript({
+    googleMapsApiKey: process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY,
+  });
 
-export default Directions
+  if (!isLoaded) return <div>Loading...</div>;
+
+  function Map() {
+    const center = useMemo(() => ({ lat: 44, lng: -88 }), []);
+
+    <GoogleMap mapContainerClassName="google-map" zoom={10} center={center}>
+      {" "}
+      <Marker position={center} />
+    </GoogleMap>;
+  }
+
+  return (
+    <Container fluid>
+      <Row>
+        <Col className="text-center">
+          <h1>Directions</h1>
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+          <Map />
+        </Col>
+        <Col>1 of 1</Col>
+        <Col>1 of 1</Col>
+      </Row>
+    </Container>
+  );
+};
+
+export default Directions;
