5ebcfd5ade77168d11bfb77d0a58eaea2a28fab1
Paul <paulmcfetridge1993@gmail.com>
2022-05-23 11:11:16 +0100

    PM: Adding relevant space filler content to the About Page

--- qa-cinemas/src/components/About/About.jsx
+++ qa-cinemas/src/components/About/About.jsx
@@ -20,37 +20,25 @@ const About = () => {
             <Accordion.Item eventKey="0">
               <Accordion.Header>Who are QA?</Accordion.Header>
               <Accordion.Body>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-                enim ad minim veniam, quis nostrud exercitation ullamco laboris
-                nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
-                in reprehenderit in voluptate velit esse cillum dolore eu fugiat
-                nulla pariatur. Excepteur sint occaecat cupidatat non proident,
-                sunt in culpa qui officia deserunt mollit anim id est laborum.
+                We are a premium supplier of Cinematic experiences, focussing 
+                heavily on customer service, food that sells itself, and comfortable
+                seating for customers to enjoy the next biggest blockbusters. 
               </Accordion.Body>
             </Accordion.Item>
             <Accordion.Item eventKey="1">
               <Accordion.Header>How can we help?</Accordion.Header>
               <Accordion.Body>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-                enim ad minim veniam, quis nostrud exercitation ullamco laboris
-                nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
-                in reprehenderit in voluptate velit esse cillum dolore eu fugiat
-                nulla pariatur. Excepteur sint occaecat cupidatat non proident,
-                sunt in culpa qui officia deserunt mollit anim id est laborum.
+                Whether it's a quick visit for a movie, or rental for an entire 
+                screen for a party or gaming night! You can reach out via our 
+                Contact page above and we can discuss your requirements. 
               </Accordion.Body>
             </Accordion.Item>
             <Accordion.Item eventKey="2">
               <Accordion.Header>Want to join us?</Accordion.Header>
               <Accordion.Body>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-                enim ad minim veniam, quis nostrud exercitation ullamco laboris
-                nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
-                in reprehenderit in voluptate velit esse cillum dolore eu fugiat
-                nulla pariatur. Excepteur sint occaecat cupidatat non proident,
-                sunt in culpa qui officia deserunt mollit anim id est laborum.
+                We are currently hiring for (0) positions. 
+                Please check back regularly if there's not a 
+                position that suits you at the moment.
               </Accordion.Body>
             </Accordion.Item>
           </Accordion>
@@ -60,16 +48,7 @@ const About = () => {
         <Col>
           <h2> Credits/Inspirations </h2>
           <p>
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolore
-            fugit alias ratione veniam hic ad in vel ipsam iste molestias
-            eligendi vitae quos, delectus dolorem aspernatur nihil totam quod
-            repudiandae. Aliquam incidunt, dolorem quos in beatae eos harum rem
-            voluptatibus cum nemo. Architecto ex, dignissimos nulla illo labore
-            vel natus quidem soluta obcaecati! Ullam accusantium, at nihil eum
-            iusto id! Dolorem perspiciatis voluptas animi quas rerum. Ea
-            molestias minima officiis laboriosam asperiores! Blanditiis nesciunt
-            voluptatum corporis beatae aperiam officia nulla recusandae numquam
-            quia. Nostrum reiciendis, enim excepturi dicta dignissimos amet.
+          Filler Text
           </p>
         </Col>
         <Col>
@@ -78,17 +57,7 @@ const About = () => {
         <Col>
           <h2> Dev Team </h2>
           <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum,
-            omnis! Illum rem libero provident eveniet inventore unde doloribus
-            vel officia voluptate similique. Cupiditate veritatis voluptatibus
-            minima necessitatibus cumque est natus? Enim tenetur nam facilis
-            voluptates modi provident ipsa voluptate impedit voluptatem pariatur
-            suscipit necessitatibus quibusdam, vero sed harum quis officia
-            numquam illum incidunt! Quae eius suscipit officiis adipisci aliquid
-            magni. Eveniet molestias quas quam commodi perferendis sint harum
-            ipsam, recusandae consectetur necessitatibus voluptate impedit
-            obcaecati. Assumenda eum quod voluptatum ex numquam magni excepturi,
-            voluptatibus neque alias harum iusto ad quibusdam.
+            A bunch of chill dudes that are just wingin' it.
           </p>
         </Col>
       </Row>
